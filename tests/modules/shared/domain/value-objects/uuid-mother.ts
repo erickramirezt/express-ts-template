f0c7afe961b48be11d1d9eb722b1d92783b71fae
Erick Ramirez <erickramirez.dnbk@gmail.com>
2024-03-14 09:54:54 -0300

    Añadido UuidMother para crear y obtener UUIDs válidos y no válidos

---
+++ tests/modules/shared/domain/value-objects/uuid-mother.ts
@@ -0,0 +1,13 @@
+import { Uuid } from '../../../../../src/modules/shared/domain/value-objects/uuid'
+import { MotherCreator } from './mother-creator/mother-creator'
+import { StringMother } from './mother-creator/string-mother'
+
+export const UuidMother = {
+  create (value?: string) {
+    return new Uuid(value ?? MotherCreator.string.uuid())
+  },
+
+  invalidUuid () {
+    return StringMother.word({ max: 10 })
+  }
+}
