101a4c58e2e3431f9633876f955076ce7b796f8b
Erick Ramirez <erickramirez.dnbk@gmail.com>
2024-04-23 07:15:42 -0400

    playwrigth added and supertest deleted

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,8 @@
 module.exports = {
-  env: {
-    es2021: true,
-    node: true
-  },
-  extends: 'standard-with-typescript',
   overrides: [
     {
-      env: {
-        node: true
-      },
-      files: ['.eslintrc.{js,cjs}'],
-      parserOptions: {
-        sourceType: 'script'
-      }
+      files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+      extends: 'love'
     }
   ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  rules: {}
 }
